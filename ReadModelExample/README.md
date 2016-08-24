131da03735c3358159e8a05479b34386ebe7ee15
Ton Schoots <ton@maiastra.com>
2016-08-24 18:08:45 +0200

    added a sample program to read a .uml file

---
+++ ReadModelExample/README.md
@@ -0,0 +1,4 @@
+# ReadUML2File
+
+The java project is in the directory javaproject , import this java project in your eclipse workspace
+Don't forget to reference all the jars in the uml2jars directory
---
+++ ReadModelExample/javaproject/src/com/maiastra/ReadUmlModel.java
@@ -0,0 +1,62 @@
+
+
+package com.maiastra;
+
+import java.io.File; 
+import java.io.FilenameFilter; 
+import java.io.IOException; 
+import java.net.URISyntaxException; 
+import java.net.URL; 
+import java.util.ArrayList; 
+import java.util.HashMap; 
+import java.util.List; 
+import java.util.Map; 
+import java.util.Properties; 
+ 
+//import org.eclipse.core.runtime.CoreException; 
+//import org.eclipse.core.runtime.FileLocator; 
+//import org.eclipse.core.runtime.Platform; 
+import org.eclipse.emf.common.util.URI; 
+import org.eclipse.emf.ecore.EAttribute; 
+import org.eclipse.emf.ecore.EClass; 
+import org.eclipse.emf.ecore.EOperation; 
+import org.eclipse.emf.ecore.EPackage; 
+import org.eclipse.emf.ecore.resource.Resource; 
+import org.eclipse.emf.ecore.resource.ResourceSet; 
+import org.eclipse.emf.ecore.resource.URIConverter; 
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl; 
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.uml2.uml.Model;
+import org.eclipse.uml2.uml.UMLPackage; 
+import org.eclipse.uml2.uml.resource.UMLResource; 
+import org.eclipse.uml2.uml.resources.util.UMLResourcesUtil; 
+//import org.osgi.framework.FrameworkUtil;
+
+public class ReadUmlModel {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hallo hallo");
+		
+		ResourceSet rSet = new ResourceSetImpl();
+		rSet.getPackageRegistry().put(UMLPackage.eNS_URI, UMLPackage.eINSTANCE);
+		rSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put(UMLResource.FILE_EXTENSION,  UMLResource.Factory.INSTANCE);
+		org.eclipse.emf.common.util.URI modelUri = URI.createURI("file:///home/tschoots/MAIASTRA_development/papyrus_java_ws/org.eclipse.uml2.examples.gettingstarted/ExtendedPO2.uml");
+		//java.net.URI modelUri = new java.net.URI("file:///home/tschoots/MAIASTRA_development/papyrus_java_ws/org.eclipse.uml2.examples.gettingstarted/ExtendedPO2.uml");
+		UMLResource r = (UMLResource) rSet.getResource(modelUri, true);
+		EcoreUtil.resolveAll(r);
+		UMLResourcesUtil.init(rSet);
+		
+		System.out.println(r.getEncoding());
+		System.out.println(r.getXMINamespace());
+		Model umlModel = (Model) r.getContents().get(0);
+		
+		System.out.println(umlModel.getName());
+		
+		
+		System.out.println("the end");
+
+
+	}
+
+}
